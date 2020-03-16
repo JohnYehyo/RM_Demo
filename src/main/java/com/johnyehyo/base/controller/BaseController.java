469b53454e3dbe71054472731b6c4aca0d4410bd
JohnYehyo <836524969@qq.com>
2020-03-16 11:54:47 +0800

    各类型消息

--- src/main/java/com/johnyehyo/base/controller/BaseController.java
+++ src/main/java/com/johnyehyo/base/controller/BaseController.java
@@ -1,6 +1,9 @@
 package com.johnyehyo.base.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.johnyehyo.base.entity.UserEntity;
+import org.junit.Test;
 import org.springframework.amqp.rabbit.connection.CorrelationData;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,6 +29,42 @@ public class BaseController {
         return "test";
     }
 
+    /**
+     * direct类型
+     */
+    @RequestMapping(value = "login")
+    public void login() {
+        UserEntity user = new UserEntity();
+        user.setPassword("123456");
+        ObjectMapper mapper = new ObjectMapper();
+        for (int i = 0; i < 10; i++) {
+            user.setUsername("测试王" + UUID.randomUUID().toString().replace("_", ""));
+            try {
+                String s = mapper.writeValueAsString(user);
+                rabbitTemplate.convertAndSend("direct-exchange", "direct-queue-key", s);
+            } catch (JsonProcessingException e) {
+                e.printStackTrace();
+            }
+
+        }
+    }
+
+    /**
+     * direct类型 使用@RabbitListener监听功能
+     */
+    @RequestMapping(value = "submit")
+    public void submit() {
+        UserEntity user = new UserEntity();
+        user.setPassword("123456");
+        for (int i = 0; i < 30; i++) {
+            user.setUsername("测试王" + UUID.randomUUID().toString().replace("_", ""));
+            rabbitTemplate.convertAndSend("direct-exchange", "direct-queue-key", user);
+        }
+    }
+
+    /**
+     * topic类型 使用@RabbitListener绑定和监听功能
+     */
     @RequestMapping(value = "submitWithReturn")
     public void submitWithReturn() {
         CorrelationData correlationData = new CorrelationData();
