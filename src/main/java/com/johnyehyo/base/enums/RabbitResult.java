b140a85aed01e3a6dce7caf456ddf1e9df74db5c
JohnYehyo <836524969@qq.com>
2020-06-18 10:25:33 +0800

    消费消息的说明

---
+++ src/main/java/com/johnyehyo/base/enums/RabbitResult.java
@@ -0,0 +1,17 @@
+package com.johnyehyo.base.enums;
+
+public enum RabbitResult {
+
+    /**
+     * 成功
+     */
+    SUCCESS,
+    /**
+     * 重试
+     */
+    RETRY,
+    /**
+     * 丢弃
+     */
+    DISCARDED
+}
--- src/main/java/com/johnyehyo/base/service/confim/comsumer/MessageConsumer6.java
+++ src/main/java/com/johnyehyo/base/service/confim/comsumer/MessageConsumer6.java
@@ -1,7 +1,10 @@
 package com.johnyehyo.base.service.confim.comsumer;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.johnyehyo.base.entity.UserEntity;
+import com.johnyehyo.base.enums.RabbitResult;
 import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.MessageProperties;
 import org.springframework.amqp.core.ExchangeTypes;
 import org.springframework.amqp.rabbit.annotation.*;
 import org.springframework.amqp.support.AmqpHeaders;
@@ -23,22 +26,53 @@ import java.util.Map;
 public class MessageConsumer6 {
 
     @RabbitListener(bindings = @QueueBinding(
-            exchange = @Exchange(value = "topic-exchange", durable = "true", type = ExchangeTypes.TOPIC),
-            value = @Queue(value = "topic-queue", durable = "true"),
-            key = "order.#"
+            value = @Queue(value = "rj2-queue", durable = "true"),
+            exchange = @Exchange(value = "test-exchange", durable = "true", type = ExchangeTypes.TOPIC),
+            key = "order.*"
     ))
     @RabbitHandler
     public void onMessage(@Payload UserEntity body, @Headers Map<String, Object> headers, Channel channel) throws IOException {
+
+//        tips:
+//        生产环境应该区分本身有误的消息体,这类消息体应该直接丢弃并记录日志而不应该再执行重入队列
+//        的过程,对于其它允许重试的消息,通过记录尝试次数当尝试次数达到设定值时同样丢弃并记录日志
+
         Long tag = (Long) headers.get(AmqpHeaders.DELIVERY_TAG);
+        String RECEIVED_EXCHANGE = (String) headers.get(AmqpHeaders.RECEIVED_EXCHANGE);
+        String RECEIVED_ROUTING_KEY = (String) headers.get(AmqpHeaders.RECEIVED_ROUTING_KEY);
+        ObjectMapper mapper = new ObjectMapper();
+        byte[] bytes = mapper.writeValueAsBytes(body);
+        RabbitResult rr = RabbitResult.RETRY;
         try {
-            System.out.println("[corfim]接收到消息: " + body);
+            System.out.println("[corfim222]接收到消息: " + body);
             //后续业务处理...
-            channel.basicAck(tag, false);
+            if(true){
+                //业务处理成功
+                rr = RabbitResult.SUCCESS;
+            }
+            Thread.sleep(3000);
         } catch (Exception e) {
+            rr = RabbitResult.DISCARDED;
             System.out.println("异常...");
             e.printStackTrace();
-            channel.basicNack(tag, false,false);
+        }finally {
+            if(rr == RabbitResult.SUCCESS){
+                channel.basicAck(tag, false);
+            }else if(rr == RabbitResult.RETRY){
+//                tips:
+//                第一种方法当消息回滚到消息队列时，这条消息不会回到队列尾部，而是仍是在队列头部会造成循
+//                环阻塞消息堆积,可以采用依然手动应答这时消息队列会删除该消息,同时再将消息发送到队列消息
+//                便回归尾部
+                //1
+                channel.basicNack(tag, false, true);
+                //2
+//                channel.basicAck(tag, false);
+//                channel.basicPublish(RECEIVED_EXCHANGE,
+//                        RECEIVED_ROUTING_KEY, MessageProperties.PERSISTENT_TEXT_PLAIN,
+//                        bytes);
+            }else{
+                channel.basicNack(tag, false, false);
+            }
         }
-
     }
 }
